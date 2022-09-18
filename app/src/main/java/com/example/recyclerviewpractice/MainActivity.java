885d4362f747e850c15fc9e6a36273aa18ef5155
Junaid Iqbal <Junaid.jigar761@gmail.com>
2022-09-18 12:05:07 +0500

    C1.03 RecyclerView accessed in MainActivity.java

--- app/src/main/java/com/example/recyclerviewpractice/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewpractice/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.recyclerviewpractice;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView mProgramingList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mProgramingList = findViewById(R.id.rv_programing_list);
     }
 }
