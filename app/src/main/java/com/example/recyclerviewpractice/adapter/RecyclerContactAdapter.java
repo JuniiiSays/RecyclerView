4167664d809757f0502e3bbafed0ac49831b7825
Junaid Iqbal <Junaid.jigar761@gmail.com>
2022-09-18 20:56:18 +0500

    Tutorial 02.04 onCreateViewHolder implemented

---
+++ app/src/main/java/com/example/recyclerviewpractice/adapter/RecyclerContactAdapter.java
@@ -0,0 +1,45 @@
+package com.example.recyclerviewpractice.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.recyclerviewpractice.R;
+
+public class RecyclerContactAdapter extends RecyclerView.Adapter<RecyclerContactAdapter.ViewHolder> {
+
+    Context mContext;
+    RecyclerContactAdapter(Context context){
+        mContext = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(mContext).inflate(R.layout.contact_row, parent, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public ViewHolder(View itemView){
+            super(itemView);
+        }
+    }
+
+}
