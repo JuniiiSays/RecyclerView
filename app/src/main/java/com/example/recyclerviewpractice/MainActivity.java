4f7d0605b91f3f0696a50b33d9af9883226fdd4f
Junaid Iqbal <Junaid.jigar761@gmail.com>
2022-09-18 17:57:45 +0500

    Tutorial 02.02 contact_row.xml file added & designed

--- app/src/main/java/com/example/recyclerviewpractice/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewpractice/MainActivity.java
@@ -6,20 +6,18 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-import com.example.recyclerviewpractice.adapter.ProgramingAdapter;
-
 public class MainActivity extends AppCompatActivity {
 
-    RecyclerView mProgramingList;
-    String[] languages = {"Java", "JavaScript", "C#", "Php", "C", "C++", "Java", "JavaScript", "C#", "Php", "C", "C++"};
+    RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mProgramingList = findViewById(R.id.rv_programing_list);
-        mProgramingList.setLayoutManager(new LinearLayoutManager(this));
-        mProgramingList.setAdapter(new ProgramingAdapter(languages));
+        mRecyclerView = findViewById(R.id.recyclerContact);
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 }
