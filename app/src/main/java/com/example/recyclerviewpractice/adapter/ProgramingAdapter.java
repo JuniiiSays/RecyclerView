ce8a208abbe47f18069053d8e7a944d837ea11dc
Junaid Iqbal <Junaid.jigar761@gmail.com>
2022-09-18 12:44:15 +0500

    C1.07 onBindViewHolder method implemented

--- app/src/main/java/com/example/recyclerviewpractice/adapter/ProgramingAdapter.java
+++ app/src/main/java/com/example/recyclerviewpractice/adapter/ProgramingAdapter.java
@@ -3,11 +3,15 @@ package com.example.recyclerviewpractice.adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.recyclerviewpractice.R;
+
+import org.w3c.dom.Text;
 
 class ProgramingAdapter extends RecyclerView.Adapter<ProgramingAdapter.ProgramingViewHolder> {
 
@@ -27,7 +31,8 @@ class ProgramingAdapter extends RecyclerView.Adapter<ProgramingAdapter.Programin
 
     @Override
     public void onBindViewHolder(@NonNull ProgramingViewHolder holder, int position) {
-
+        String title = data[position];
+        holder.listItemTextView.setText(title);
     }
 
     @Override
@@ -37,8 +42,13 @@ class ProgramingAdapter extends RecyclerView.Adapter<ProgramingAdapter.Programin
 
     public class ProgramingViewHolder extends RecyclerView.ViewHolder{
 
+        ImageView listItemImageView;
+        TextView listItemTextView;
+
         public ProgramingViewHolder(@NonNull View itemView) {
             super(itemView);
+            listItemImageView = itemView.findViewById(R.id.list_item_image_view);
+            listItemTextView = itemView.findViewById(R.id.list_item_text_view);
         }
     }
 
